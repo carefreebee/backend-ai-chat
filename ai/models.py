23cf1918eb5e288ac9116f53ab8eb18fa10415a9
carefreebee <leonelchristiebaclayon@gmail.com>
2024-05-21 15:51:12 +0800

    changes. unta di maguba

--- ai/models.py
+++ ai/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Chat(models.Model):
+    chat = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'chat': self.chat,
+            'created_at': self.created_at.isoformat(),
+            'user_id': self.user_id,
+        }
--- ai_chat_assistant_backend/wsgi.py
+++ ai_chat_assistant_backend/wsgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+import asyncio
+import platform
+
+# Set event loop policy based on platform
+if platform.system() != 'Windows':
+    asyncio.set_event_loop_policy(asyncio.DefaultEventLoopPolicy())
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ai_chat_assistant_backend.settings')
 
